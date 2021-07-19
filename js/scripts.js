8b2d51f89867398b08950dd6b3346e70255c8f14
Kiernan2 <kiernan1994@gmail.com>
2021-07-19 15:45:24 -0700

    Now with loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a heading.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
+  const tags = ["h1", "p", "img"];
+  tags.forEach(function (element) {
+    $(element).click(function () {
+      alert("This is a " + element + " tag.");
+    });
   });
 });
